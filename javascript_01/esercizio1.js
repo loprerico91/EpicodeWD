4df664e58fc7c8b7560fd51aa6dc4778bf44481d
Federico Lo Presti <150164610+loprerico91@users.noreply.github.com>
2023-12-12 14:46:53 +0100

    Add files via upload

---
+++ javascript_01/esercizio1.js
@@ -0,0 +1,7 @@
+let algoritmo =  " l'algoritmo è quella sequenza di istruzioni che sapientemente date, possiamo ottenere i risultati desiderati ";
+let variabile = " è un contenitore con contenuto variabile ";
+let undefined = " si assegna quando non sappiamo cosa c'è nel contenitore ";
+let nullO = " viene utilizzato per dire che quel contenitore è vuoto "; 
+let provaConsole = "ciao sono una prova";
+
+console.log(nullO)
