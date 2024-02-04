f9452c28883d6e0a1925507d9961c3aa0f7152bd
Federico Lo Presti <150164610+loprerico91@users.noreply.github.com>
2024-02-04 15:46:02 +0100

    Add files via upload

---
+++ web.js
@@ -0,0 +1,21 @@
+
+
+function getInputValue() {
+    
+    const inputElement = document.querySelector('input[type="text"]');
+  
+   
+    if (!inputElement) {
+      return;
+    }
+  
+    
+    const inputValue = inputElement.value;
+  
+    
+    console.log(inputValue);
+  };
+  
+  
+
+
